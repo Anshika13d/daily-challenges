496bbdf792a688743c84f51d33d9f880a55abfdb
Anshika <116324398+Anshika13d@users.noreply.github.com>
2024-10-07 20:43:46 +0530

    oct 7, 2024

---
+++ Strings/Minimum_string_length_after_removing_substrings.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minLength(String s) {
+        int n = s.length();
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch: s.toCharArray()){
+            if(ch == 'B' && !stack.isEmpty() && stack.peek() == 'A'){
+                stack.pop();
+            }
+            else if(ch == 'D' && !stack.isEmpty() && stack.peek() == 'C'){
+                stack.pop();
+            }
+            else{
+                stack.push(ch);
+            }
+        }
+
+        return stack.size();
+    }
+}
